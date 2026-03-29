5a75a1d78d438b0b7206df00ce90d49117275604
Glaezz <fiezarausyan@gmail.com>
2026-03-29 07:13:34 +0700

    fixing lint on build

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   output: 'export',
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
 };
 
 export default nextConfig;
